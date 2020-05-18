981140efac7e989d4e5497b4f79c7322b48e80c9
devfromfinland <contact@vietphan.info>
2020-05-18 22:01:46 +0300

    feat: add History component and Tab Navigation

--- App.js
+++ App.js
@@ -1,15 +1,67 @@
 import React from 'react'
-import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
+import { View, Platform, Text, StatusBar } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import { purple, white } from './utils/colors'
+import { NavigationContainer} from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import AddEntry from './components/AddEntry'
+import History from './components/History'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo-constants'
+
+function MyStatusBar ({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: 20}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tab = Platform.OS === 'ios'
+  ? createBottomTabNavigator()
+  : createMaterialTopTabNavigator()
+
+const tabBarOptions = {
+  activeTintColor: Platform.OS === 'ios' ? purple : white,
+  style: {
+    height: 56,
+    backgroundColor: Platform.OS === 'ios' ? white : purple,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 1,
+  },
+  inactiveTintColor: 'gray',
+}
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
-        <AddEntry />
+        <MyStatusBar backgroundColor={purple} barStyle='light'/>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color }) => {
+                if (route.name === 'History') {
+                  return <Ionicons name='ios-bookmarks' size={30} color={color} />
+                } else if (route.name === 'Add Entry') {
+                  return <FontAwesome name='plus-square' size={30} color={color} />
+                }
+              },
+            })}
+            tabBarOptions={tabBarOptions}
+          >
+            <Tab.Screen name='History' component={History} />
+            <Tab.Screen name='Add Entry' component={AddEntry} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </View>
     </Provider>
   )
